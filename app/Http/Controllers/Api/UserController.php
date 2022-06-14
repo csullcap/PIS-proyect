1d272affe27009c0c99ff47e3332142991bf9848
Christian Dacid Sullca Puma <csullcap@unsa.edu.pe>
2022-06-13 20:21:39 -0500

    V1.0.0

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|confirmed'
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            'message'=>'registro exitoso'
+        ]);
+    }
+
+    public function login(Request $request){
+
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        $user = User::where("email","=", $request->email)->first();
+
+        if(isset($user->id) ){
+            if(Hash::check($request->password, $user->password)){
+                $token = $user->createToken("auth_token")->plainTextToken;
+                return response()->json([
+                    "msg" => "Usuario logueado",
+                    "access_token" => $token
+                ]);        
+            }else{
+                return response()->json([
+                    "msg" => "La password es incorrecta",
+                ], 404);    
+            }
+
+        }else{
+            return response()->json([
+                "status" => 0,
+                "msg" => "Usuario no registrado",
+            ], 404);  
+        }
+    }
+
+    public function userProfile(){
+        return response()->json([
+            "status" => 0,
+            "msg" => "Perfil de usuario",
+            "data" => auth()->user()
+        ]); 
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "status" => 1,
+            "msg" => "Cierre de Sesión",            
+        ]); 
+    }
+    
+}
