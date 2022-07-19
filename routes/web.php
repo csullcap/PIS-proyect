73cbb03dedb4190cf02e5b84890ccabd3ec1d9e9
Christian Dacid Sullca Puma <csullcap@unsa.edu.pe>
2022-07-19 02:54:16 -0500

    configuraciones de despliegue

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['APP' => 'Sistema Gestor de Calidad de la Escuela de Relaciones Industriales'];
 });
