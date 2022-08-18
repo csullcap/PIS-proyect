527ed5d964ae8f642d1c1cd2609016ee0b95ff87
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-08-18 12:22:25 -0500

    Cors Google

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*','localhosto:3000/*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['Google/'],
 
     'allowed_headers' => ['*'],
 
