46eb9de38492b38b072fb72779634dc69a19eb8d
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-10-07 19:36:20 -0500

    Model Narrativa

---
+++ app/Models/Narrativa.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Narrativa extends Model
+{
+    use HasFactory;
+
+	protected $table ='narrativas';
+    protected $fillable = [
+        'year',
+        'semestre',
+        'cabecera',
+		'contenido',
+    ];
+
+	public function estandars(){
+        return $this->belongsTo(Estandar::class,'id_estandar');
+    }
+}
