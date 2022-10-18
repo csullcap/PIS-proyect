827ff6e7c7dc220a67f4581cbacc81fec97a7a31
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-10-18 00:05:57 -0500

    Modified create cabecera controller

--- app/Http/Controllers/Api/EstandarController.php
+++ app/Http/Controllers/Api/EstandarController.php
@@ -71,6 +71,7 @@ class EstandarController extends Controller
         if(Estandar::where(["id_user"=>$id_user,"id"=>$id])->exists()){
             $estandar = Estandar::find($id);
             $estandar->name = isset($request->name) ? $request->name : $estandar->name;
+			$estandar->cabecera = isset($request->cabecera) ? $request->cabecera : $estandar->cabecera;
             $estandar->save();
             return response([
                 "status" => 1,
